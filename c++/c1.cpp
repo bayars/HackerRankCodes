402b2273206b5b0e80fe2d4b37add9bcbd0db1a3
Safa Bayar <sbayar2012@outlook.com>
2018-10-10 14:56:50 -0400

    up

---
+++ c++/c1.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int>a;
+vector<int>b;
+int main() {
+	int l,m,input;
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	//cin >> l >> m;
+	while (cin >> input || a[0] != a.size()){
+		a.push_back(input);
+	}
+	cout << a[0];
+			        
+	return 1;
+}
