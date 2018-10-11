efdf87f0738c0d397fa925f6f6cadd3ab8fb34e7
Safa Bayar <sbayar2012@outlook.com>
2018-10-11 11:03:30 -0400

    derlendi.

--- c++/ClassAndObjects.cpp
+++ c++/ClassAndObjects.cpp
@@ -67,21 +67,7 @@ class Student{
         }
 };
 
-// Write your Student class here
-class Student{
-    private:
-    int scores[5];
-    int sum;
-    public:
-        Student() : sum(0) {}
-        int calculateTotalScore() {return sum;}
-        void input() {
-            for(int i=0; i<5; i++) {
-                cin >> scores[i];
-                sum+=scores[i];
-            }
-        }
-};
+
 int main() {
     int n; // number of students
     cin >> n;
