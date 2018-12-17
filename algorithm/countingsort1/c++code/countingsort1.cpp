b209f6bee5f8e213d45e05378bd81396a5d03da3
rection <sbayar2012@outlook.com>
2018-12-18 00:59:38 +0300

    add counting sort

---
+++ algorithm/countingsort1/c++code/countingsort1.cpp
@@ -0,0 +1,170 @@
+/*
+Hacker rank link: https://www.hackerrank.com/challenges/countingsort1/problem
+
+Comparison Sorting
+Quicksort usually has a running time of , but is there an algorithm that can sort even faster? In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. A comparison sort algorithm cannot beat (worst-case) running time, since
+
+represents the minimum number of comparisons needed to know where to place each element. For more details, you can see these notes (PDF).
+
+Alternative Sorting
+Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+
+For example, consider an array
+. All of the values are in the range , so create an array of zeroes,
+
+. The results of each iteration follow:
+
+i	arr[i]	result
+0	1	[0, 1, 0, 0]
+1	1	[0, 2, 0, 0]
+2	3	[0, 2, 0, 1]
+3	2	[0, 2, 1, 1]
+4	1	[0, 3, 1, 1]
+
+Now we can print the list of occurrences,
+or determine the sorted array:
+
+.
+
+Challenge
+Given a list of integers, count and output the number of times each value appears as a list of space-separated integers.
+
+Function Description
+
+Complete the countingSort function in the editor below. It should return an array of integers where each value is the number of occurrences of the element's index value in the original array.
+
+countingSort has the following parameter(s):
+
+    arr: an array of integers
+
+Input Format
+
+The first line contains an integer
+, the number of items in .
+Each of the next lines contains an integer where
+
+.
+
+Constraints
+
+
+Output Format
+
+Output the number of times every number from
+through appears in
+
+as a list of space-separated integers on one line.
+
+Sample Input
+
+100
+63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33
+
+Sample Output
+
+0 2 0 2 0 0 1 0 1 2 1 0 1 1 0 0 2 0 1 0 1 2 1 1 1 3 0 2 0 0 2 0 3 3 1 0 0 0 0 2 2 1 1 1 2 0 2 0 1 0 1 0 0 1 0 0 2 1 0 1 1 1 0 1 0 1 0 2 1 3 2 0 0 2 1 2 1 0 2 2 1 2 1 2 1 1 2 2 0 3 2 1 1 0 1 1 1 0 2 2
+
+Explanation
+
+Each of the resulting values
+represents the number of times appeared in .
+*/
+
+/*
+
+It's doesn't work now but works only first and second test. Hacker rank say congrulations what i made. it's don't work for big decimal number give segmentaion fault error. Vector size 10000 should be require too much memory.
+
+*/
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the countingSort function below.
+vector<int> countingSort(vector<int> arr) {
+    map<int,int> number_occurence;
+
+    for(int i : arr) number_occurence[i]++;
+
+    auto it = number_occurence.end();
+    it--;
+
+    arr.resize(it->first + 1);
+
+    for(int i = 0; i < arr.size(); i++)
+        arr[i] = number_occurence[i];
+
+    return arr;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = countingSort(arr);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
---
+++ algorithm/countingsort1/ccode/countingsort1.c
@@ -0,0 +1,214 @@
+/*
+Hacker rank problem link: https://www.hackerrank.com/challenges/countingsort1/problem
+
+Comparison Sorting
+Quicksort usually has a running time of , but is there an algorithm that can sort even faster? In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. A comparison sort algorithm cannot beat (worst-case) running time, since
+
+represents the minimum number of comparisons needed to know where to place each element. For more details, you can see these notes (PDF).
+
+Alternative Sorting
+Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+
+For example, consider an array
+. All of the values are in the range , so create an array of zeroes,
+
+. The results of each iteration follow:
+
+i	arr[i]	result
+0	1	[0, 1, 0, 0]
+1	1	[0, 2, 0, 0]
+2	3	[0, 2, 0, 1]
+3	2	[0, 2, 1, 1]
+4	1	[0, 3, 1, 1]
+
+Now we can print the list of occurrences,
+or determine the sorted array:
+
+.
+
+Challenge
+Given a list of integers, count and output the number of times each value appears as a list of space-separated integers.
+
+Function Description
+
+Complete the countingSort function in the editor below. It should return an array of integers where each value is the number of occurrences of the element's index value in the original array.
+
+countingSort has the following parameter(s):
+
+    arr: an array of integers
+
+Input Format
+
+The first line contains an integer
+, the number of items in .
+Each of the next lines contains an integer where
+
+.
+
+Constraints
+
+
+Output Format
+
+Output the number of times every number from
+through appears in
+
+as a list of space-separated integers on one line.
+
+Sample Input
+
+100
+63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33
+
+Sample Output
+
+0 2 0 2 0 0 1 0 1 2 1 0 1 1 0 0 2 0 1 0 1 2 1 1 1 3 0 2 0 0 2 0 3 3 1 0 0 0 0 2 2 1 1 1 2 0 2 0 1 0 1 0 0 1 0 0 2 1 0 1 1 1 0 1 0 1 0 2 1 3 2 0 0 2 1 2 1 0 2 2 1 2 1 2 1 1 2 2 0 3 2 1 1 0 1 1 1 0 2 2
+
+Explanation
+
+Each of the resulting values
+represents the number of times appeared in .
+*/
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the countingSort function below.
+
+// Please store the size of the integer array to be returned in result_count pointer. For example,
+// int a[3] = {1, 2, 3};
+//
+// *result_count = 3;
+//
+// return a;
+//
+int* countingSort(int arr_count, int* arr, int* result_count) {
+  int arr1[arr_count],w;
+  for(w=0;w<arr_count;w++){
+    arr1[w]=arr[w];
+  }
+  int count=0;
+  int sizes = arr_count;
+  int p = 0,z=0;
+  for(p=0;p<sizes;p++){
+    for(z=0;z<sizes;z++){
+      if(p == arr[z]){
+    count++;
+      }
+    }
+    arr1[p]=count;
+    count=0;
+  }
+  for(w=0;w<arr_count;w++){
+    arr[w] = arr1[w];
+  }
+  *result_count = arr_count;
+  return arr;
+}
+
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* n_endptr;
+    char* n_str = readline();
+    int n = strtol(n_str, &n_endptr, 10);
+
+    if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    char** arr_temp = split_string(readline());
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        char* arr_item_endptr;
+        char* arr_item_str = *(arr_temp + i);
+        int arr_item = strtol(arr_item_str, &arr_item_endptr, 10);
+
+        if (arr_item_endptr == arr_item_str || *arr_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        *(arr + i) = arr_item;
+    }
+
+    int arr_count = n;
+
+    int result_count;
+    int* result = countingSort(arr_count, arr, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%d", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, " ");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
